3d4fc41a7f986b53618638031a786204fd16f90a
Juan Silva <62386697+Juanzit@users.noreply.github.com>
2022-01-12 19:29:46 -0300

    Add files via upload

---
+++ Guardando arquivo.py
@@ -0,0 +1,13 @@
+import tweepy as tw
+import requests
+res = requests.get("https://pypi.org/project/selenium/")
+
+try:
+    res.raise_for_status()
+    print("Check")
+    file = open("Selenium.txt", "wb")
+    for chunck in res.iter_content(100000):
+        file.write(chunck)
+
+except Exception as exc:
+    print("Error:%s" % (exc))
